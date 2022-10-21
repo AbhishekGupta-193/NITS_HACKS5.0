d2aca5c621e85d4f83bfa187af5713daa1dfa59a
ASB1704 <singhanurag17042002@gmail.com>
2022-10-21 21:24:22 +0530

    Landing page completed sir

--- task_manager_MERN-main/nits_hacks_project/src/Components/frontend_/Landing/Landing.js
+++ task_manager_MERN-main/nits_hacks_project/src/Components/frontend_/Landing/Landing.js
@@ -3,19 +3,30 @@ import { Link, useNavigate } from 'react-router-dom'
 import {RiTaskFill} from 'react-icons/ri'
 import { Button } from '@mui/material'
 import './Landing.css'
+import { Vission } from './Vission'
+import { Footer } from './Footer'
+const src = "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/National_Institute_Of_Technology_Silchar_Logo.svg/1200px-National_Institute_Of_Technology_Silchar_Logo.svg.png"
 const Landing = () => {
      const navigate = useNavigate();
   return (
+  <>
     <div className='LandingWrapper'>
+     <div className='nitImage'>
+          <img src={src} alt="image"/>
+     </div>
      <div className="landingInfo">
           <h1>Welcome to</h1>
-          <h5>Task Manager <RiTaskFill/>  </h5>
+          <h5>GreenNITS </h5>
           <div className="signIn_signUp">
                <Button onClick={()=>navigate('/login')} variant="contained">sign In</Button>
                <Button onClick={()=>navigate('/register')} variant="contained">sign Up</Button>
           </div>
      </div>
     </div>
+    <Vission/>
+    <hr/>
+    <Footer/>
+  </>
   )
 }
 
